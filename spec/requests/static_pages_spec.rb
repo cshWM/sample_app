578f5acafdd4250073c417159ce81ebb8d21ad27
césar silva <cesar.silva@wiremaze.com>
2014-05-04 15:53:52 +0100

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  # it "should have the right links on the layout" do
+  #   visit root_path
+  #   click_link "About"
+  #   expect(page).to have_title(full_title('About Us'))
+  #   click_link "Help"
+  #   expect(page).to help_path
+  #   click_link "Contact"
+  #   expect(page).to contact_path
+  #   click_link "Home"
+  #   click_link "Sign up now!"
+  #   expect(page).to 'users/new'
+  #   click_link "sample app"
+  #   expect(page).to '#'
+  # end
+
   subject { page }
 
   describe "Home page" do
