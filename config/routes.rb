4c0441144d917fd4d10d978ed770e86373c24623
césar silva <cesar.silva@wiremaze.com>
2014-05-05 00:32:53 +0100

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root 'static_pages#home'
   match '/',   to: 'static_pages#home',   via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/signup', to: 'users#new', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
 #  get 'static_pages/home'
 #  get 'static_pages/help'
